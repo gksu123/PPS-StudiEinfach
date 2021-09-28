d1bcb4f65f624a6fca3987479ea76a9e34f49ff6
Kimberly Maria Plackenhohn (inf1359) <kimberly_maria.plackenhohn@smail.th-koeln.de>
2021-09-28 20:10:44 +0200

    Bearbeitet

--- Coding/js/fortschrittsanzeige.js
+++
@@ -1,7 +0,0 @@
-function aktualisiere_progressbar() {
-	var anteil = 0;
-	for (var k = 0; k < document.forms[0].elements.length; k++) {
-         if (document.forms[0].elements[k].value != '') anteil++; 
-        }
-	document.getElementById('fortschritt').value = anteil;
-}
---
+++ Coding/js/gelernt_Home.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    $('.minus').click(function () {
+        var $input = $(this).parent().find('input');
+        var truncated = Math.ceil(parseFloat($input.val() - 1));
+        const runden = truncated.toFixed(1)
+        var count = runden;
+        count = count < 0 ? 0 : count;
+        $input.val(count);
+        $input.change();
+        return false;
+    });
+    $('.plus').click(function () {
+        var $input = $(this).parent().find('input');
+        var truncated1 = Math.ceil(parseFloat($input.val() + 1));
+        const runden1 = truncated1.toFixed(1)
+        var count1 = runden1;
+        $input.val(count1);
+        $input.change();
+        return false;
+    });
+    
+});
+
+
+
+
+
