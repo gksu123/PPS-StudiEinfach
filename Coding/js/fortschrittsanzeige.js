7d5fe99287946f29e464bf823b8dfcba32a1fe59
Kimberly Maria Plackenhohn (inf1359) <kimberly_maria.plackenhohn@smail.th-koeln.de>
2021-10-06 14:23:03 +0200

    Fortschrittsanzeige

--- Coding/js/fortschrittsanzeige.js
+++ Coding/js/fortschrittsanzeige.js
@@ -1,7 +1,38 @@
+/*
+
 function aktualisiere_progressbar() {
 	var anteil = 0;
 	for (var k = 0; k < document.forms[0].elements.length; k++) {
          if (document.forms[0].elements[k].value != '') anteil++; 
         }
 	document.getElementById('fortschritt').value = anteil;
-}
+}*/
+
+$(document).ready(function() {
+  
+	// get box count
+	var count = 0;
+	var checked = 0;
+	function countBoxes() { 
+	  count = $("input[type='checkbox']").length;
+	  console.log(count);
+	}
+	
+	countBoxes();
+	$(":checkbox").click(countBoxes);
+	
+	// count checks
+	
+	function countChecked() {
+	  checked = $("input:checked").length;
+	  
+	  var percentage = parseInt(((checked / count) * 100),10);
+	  $(".progressbar-bar").progressbar({
+			  value: percentage
+		  });
+	  $(".progressbar-label").text(percentage + "%");
+	}
+	
+	countChecked();
+	$(":checkbox").click(countChecked);
+  });
